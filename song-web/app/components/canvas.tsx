93fd02ad50431ee7c6f23b2a1b976c93abc33ed2
Yinwhe <wyffeiwhe@gmail.com>
2024-07-27 04:33:02 +0800

    updates

--- song-web/app/components/canvas.tsx
+++ song-web/app/components/canvas.tsx
@@ -1,8 +1,19 @@
 'use client'
 
-import { useEffect, useRef, useState } from "react";
+import { useEffect, useRef, useState, createContext, useContext, MutableRefObject } from "react";
 import { Canvas, useFrame, useLoader, useThree } from "@react-three/fiber";
-import { TextureLoader, Sprite as ThreeSprite, Raycaster, Vector2, Vector3 } from "three";
+import { Texture, TextureLoader, Sprite as ThreeSprite, Raycaster, Intersection, Vector2, Vector3 } from "three";
+
+interface CanvasStatus {
+  isLoaded: boolean;
+  isFinalSelected: boolean;
+  rotationSpeed: number;
+  initCubeSize: number;
+  cubeScale: number;
+  selectedID: number;
+}
+
+const CanvasContext = createContext<MutableRefObject<CanvasStatus>>({} as MutableRefObject<CanvasStatus>);
 
 interface SpriteProps {
   initialPosition: [number, number, number];
@@ -13,23 +24,24 @@ interface SpriteProps {
 const Sprite: React.FC<SpriteProps> = ({ initialPosition, imagePath, opacity }) => {
   const texture = useLoader(TextureLoader, imagePath);
   const spriteRef = useRef<ThreeSprite>(null);
-  const rotationSpeed = 0.1;
+  const canvasStatus = useContext(CanvasContext);
 
-  // 维护当前位置状态
-  const position = useRef(new Vector3(...initialPosition));
+  const targetPosition = useRef(new Vector3(0, 0, 0));
+  const radius = (initialPosition[0] ** 2 + initialPosition[1] ** 2) ** 0.5;
+  const angle = useRef(Math.atan2(initialPosition[1], initialPosition[0]));
 
-  useFrame(({ camera, clock }) => {
+  useFrame(({ camera }) => {
     if (spriteRef.current) {
-      const elapsedTime = clock.getElapsedTime();
-      const angle = elapsedTime * rotationSpeed;
+      angle.current += 1 / 60 * canvasStatus.current.rotationSpeed;
 
       // 更新位置
-      position.current.x = initialPosition[0] * Math.cos(angle) - initialPosition[1] * Math.sin(angle);
-      position.current.y = initialPosition[0] * Math.sin(angle) + initialPosition[1] * Math.cos(angle);
-      position.current.z = initialPosition[2];
+      targetPosition.current.x = radius * Math.cos(angle.current) * canvasStatus.current.cubeScale;
+      targetPosition.current.y = radius * Math.sin(angle.current) * canvasStatus.current.cubeScale;
+      targetPosition.current.z = (initialPosition[2]) * canvasStatus.current.cubeScale;
+
+      spriteRef.current.position.lerp(targetPosition.current, 0.1);
 
       // 应用新的位置
-      spriteRef.current.position.copy(position.current);
       spriteRef.current.lookAt(camera.position);
     }
   });
@@ -37,23 +49,147 @@ const Sprite: React.FC<SpriteProps> = ({ initialPosition, imagePath, opacity }) 
   // 设置 Sprite 的 scale 以保持原始图片比例
   useEffect(() => {
     if (texture) {
-      spriteRef.current!.scale.set(texture.image.width / 1200, texture.image.height / 1200, 1);
+      spriteRef.current!.scale.set(texture.image.width / 1500, texture.image.height / 1500, 1);
     }
   }, [texture]);
 
   return (
     <sprite ref={spriteRef} position={initialPosition}>
-      <spriteMaterial attach="material" map={texture} transparent opacity={opacity} />
+      <spriteMaterial attach="material" map={texture} transparent opacity={opacity} alphaTest={0.2}/>
     </sprite>
   );
 };
 
 
+// 方向枚举
+enum CubeFace {
+  Front,
+  Back,
+  Left,
+  Right,
+  Top,
+  Bottom
+}
+
+function mapToCubeFace(locations: [number, number][], face: CubeFace, side: number): [number, number, number][] {
+  const maxDimension = 300;
+  const halfSide = side / 2;
+  const mappedLocations: [number, number, number][] = [];
+
+  locations.forEach(location => {
+    const [x2D, y2D] = location;
+
+    // 将二维坐标从左上角原点转换为立方体面中心为原点
+    const x = ((x2D / maxDimension) * side) - halfSide; // 现在范围是 [-side/2, side/2]
+    const y = ((y2D / maxDimension) * side) - halfSide; // 现在范围是 [-side/2, side/2]
+
+    let point3D: [number, number, number];
+
+    switch (face) {
+      case CubeFace.Front:
+        point3D = [x, -y, halfSide];
+        break;
+      case CubeFace.Back:
+        point3D = [-x, -y, -halfSide];
+        break;
+      case CubeFace.Left:
+        point3D = [-halfSide, -y, x];
+        break;
+      case CubeFace.Right:
+        point3D = [halfSide, -y, -x];
+        break;
+      case CubeFace.Top:
+        point3D = [x, -halfSide, -y];
+        break;
+      case CubeFace.Bottom:
+        point3D = [x, halfSide, y];
+        break;
+    }
+
+    mappedLocations.push(point3D);
+  });
+
+  return mappedLocations;
+}
+
+function loadOneFace(face: CubeFace, side: number): JSX.Element[] {
+  const locations = spriteLocations[0];
+  const mappedLocations = mapToCubeFace(locations, face, side);
+
+  return mappedLocations.map((location, index) => {
+    const [x, y, z] = location;
+    const imagePath = `/intro/${index + 1}.png`;
+
+    return <Sprite key={index} initialPosition={[x, y, z]} imagePath={imagePath} opacity={0.6} />;
+  });
+}
+
+const SpriteController = () => {
+  const canvasStatus = useContext(CanvasContext);
+  const initCubeSide = canvasStatus.current.initCubeSize;
+
+  const sprites = loadOneFace(CubeFace.Front, initCubeSide);
+  sprites.push(...loadOneFace(CubeFace.Back, initCubeSide));
+  sprites.push(...loadOneFace(CubeFace.Left, initCubeSide));
+  sprites.push(...loadOneFace(CubeFace.Right, initCubeSide));
+  sprites.push(...loadOneFace(CubeFace.Top, initCubeSide));
+  sprites.push(...loadOneFace(CubeFace.Bottom, initCubeSide));
+
+  return sprites;
+}
+
+function getActualIntersect(intersects: Intersection[]): Intersection | null {
+  for (const intersect of intersects) {
+    const obj = intersect.object;
+    const uv = intersect.uv;
+
+    if (obj instanceof ThreeSprite && obj.material.map && uv) {
+      const texture = obj.material.map as Texture;
+      if (isTransparent(texture, uv)) {
+        continue; // 透明区域，忽略此交点
+      }
+    }
+
+    return intersect;
+  }
+
+  return null; // 未找到有效交点
+}
+
+function isTransparent(texture: Texture, uv: Vector2): boolean {
+  const canvas = document.createElement('canvas');
+  const context = canvas.getContext('2d');
+
+  canvas.width = texture.image.width;
+  canvas.height = texture.image.height;
+  context?.drawImage(texture.image, 0, 0, texture.image.width, texture.image.height);
+
+  const x = Math.floor(uv.x * texture.image.width);
+  const y = Math.floor((1 - uv.y) * texture.image.height);
+  const pixel = context?.getImageData(x, y, 1, 1).data;
+
+  if (pixel) {
+    return pixel[3] < 128; // Alpha 小于 128 认为是透明
+  }
+
+  return true; // 如果无法获取像素数据，默认为透明
+}
+
+
 const CameraController = () => {
-  const { camera } = useThree();
-  const mouse = useRef({ x: 0, y: 0 });
+  const { scene, camera } = useThree();
+  const canvasStatus = useContext(CanvasContext);
+
+  // For select
+  const mouse = useRef(new Vector2());
+  const raycaster = new Raycaster();
+
   const radius = 5; // 摄像机围绕原点的半径
   const targetPosition = useRef(new Vector3(0, radius, 0)); // 目标位置
+
+  const lastSelected = useRef<ThreeSprite | null>(null);
+  const exit_select_timeout = useRef<NodeJS.Timeout | null>(null);
+  const steady_select_timeout = useRef<NodeJS.Timeout | null>(null);
 
   useEffect(() => {
     const handleMouseMove = (event: MouseEvent) => {
@@ -61,6 +197,70 @@ const CameraController = () => {
       // 将鼠标位置归一化到 [-1, 1]
       mouse.current.x = (event.clientX / innerWidth) * 2 - 1;
       mouse.current.y = -(event.clientY / innerHeight) * 2 + 1;
+
+      // 选择判断
+      raycaster.setFromCamera(mouse.current, camera);
+      const intersects = raycaster.intersectObjects(scene.children);
+      const intersect = getActualIntersect(intersects);
+      if (intersect) {
+        const obj = intersect!.object as ThreeSprite;
+
+        if (lastSelected.current) {
+          // 正在选择状态
+          if (obj != lastSelected.current) {
+            // 选择了新对象
+            canvasStatus.current.selectedID = obj.id;
+
+            lastSelected.current.material.opacity = 0.6;
+            lastSelected.current = obj;
+            obj.material.opacity = 1;
+
+            // 切换选择
+            clearTimeout(steady_select_timeout.current!);
+            steady_select_timeout.current = setTimeout(() => {
+              canvasStatus.current.isFinalSelected = true;
+            }, 3000);
+          } else {
+            // 原始对象，保持
+            
+          }
+
+        } else {
+          // 进入选择状态
+          canvasStatus.current.rotationSpeed = 0.02;
+          canvasStatus.current.cubeScale = 1.5;
+          canvasStatus.current.selectedID = obj.id;
+
+          obj.material.opacity = 1;
+
+          lastSelected.current = obj;
+
+          // 避免不恰当的 Timeout
+          clearTimeout(exit_select_timeout.current!);
+
+          // 准备选择稳定
+          steady_select_timeout.current = setTimeout(() => {
+            canvasStatus.current.isFinalSelected = true
+          }, 3000);
+        }
+      } else {
+        if (lastSelected.current) {
+          // 退出选择
+          const obj = lastSelected.current;
+          obj.material.opacity = 0.6;
+
+          exit_select_timeout.current = setTimeout(() => {
+            if (!lastSelected.current) {
+              canvasStatus.current.rotationSpeed = 0.1;
+              canvasStatus.current.cubeScale = 1;
+              canvasStatus.current.selectedID = -1;
+            }
+          }, 2000)
+
+          lastSelected.current = null;
+        } else {
+        }
+      }
     };
 
     window.addEventListener('mousemove', handleMouseMove);
@@ -88,48 +288,48 @@ const CameraController = () => {
 };
 
 
+const spriteLocations: [number, number][][] = [
+  // One Face
+  [
+    [38.17, 28.54],
+    [75.03, 97.75],
+    [21.96, 210.47],
+    [38.17, 227.19],
+    [143.23, 47.44],
+    [167.96, 97.75],
+    [117.17, 114.0],
+    [191.05, 172.7],
+    [150.00, 227.1],
+    [91.91, 277.19],
+    [260.05, 28.54],
+    [265.46, 118.4],
+    [250.08, 201.9],
+    [225.03, 201.0],
+    [206.77, 284.8],
+    [276.11, 277.1],
+  ],
+]
+
 export const IntroCanvas = () => {
-  // const numSprites = 64; // 定义 sprite 数量
-  // const baseRadius = 1.5;  // 基础半径
-  // const variation = 0.5; // 半径变化量
-
-  // // 生成球面上的点
-  // const sprites = Array.from({ length: numSprites }, (_, index) => {
-  //   const phi = Math.acos(-1 + 2 * Math.random()); // [-1, 1] 范围内随机
-  //   const theta = 2 * Math.PI * Math.random(); // [0, 2π] 范围内随机
-  //   const radius = baseRadius + Math.random() * variation; // 为每个 sprite 分配不同的半径
-
-  //   const x = radius * Math.sin(phi) * Math.cos(theta);
-  //   const y = radius * Math.sin(phi) * Math.sin(theta);
-  //   const z = radius * Math.cos(phi);
-  //   const imagePath = `/intro/${index + 1}.png`; // 假设图片存放在 public/intro 文件夹
-
-  //   return <Sprite key={index} initialPosition={[x, y, z]} imagePath={imagePath} opacity={0.6} />;
-  // });
-
-  const numSprites = 64;
-  const cubeSize = 3; // 立方体的边长，从 -1 到 1 的范围
-
-  // 生成立方体内的点
-  const sprites = Array.from({ length: numSprites }, (_, index) => {
-    const face = Math.floor(Math.random() * 6);
-    const x = (face === 0 ? 1 : face === 1 ? -1 : (Math.random() * 2 - 1)) * cubeSize / 2;
-    const y = (face === 2 ? 1 : face === 3 ? -1 : (Math.random() * 2 - 1)) * cubeSize / 2;
-    const z = (face === 4 ? 1 : face === 5 ? -1 : (Math.random() * 2 - 1)) * cubeSize / 2;
-    const imagePath = `/intro/${index + 1}.png`; // 假设图片存放在 public/intro 文件夹
-
-    return <Sprite key={index} initialPosition={[x, y, z]} imagePath={imagePath} opacity={0.6} />;
+  const canvasStatus = useRef<CanvasStatus>({
+    isLoaded: false,
+    isFinalSelected: false,
+    rotationSpeed: 0.1,
+    initCubeSize: 2.5,
+    cubeScale: 1,
+    selectedID: -1,
   });
 
-  return (<><Canvas
+
+  return (<CanvasContext.Provider value={canvasStatus}><Canvas
     style={{ position: 'absolute', top: 0, left: 0, width: '100%', height: '100%', backgroundColor: 'black' }}
     camera={{ position: [5, 0, 0], up: [0, 0, 1] }}
   >
     <ambientLight intensity={0.3} />
     <pointLight position={[10, 10, 10]} />
     {/* <axesHelper args={[10]} /> */}
+    <SpriteController />
     <CameraController />
-    {sprites}
-  </Canvas></>
+  </Canvas></CanvasContext.Provider>
   )
 }
--- song-web/app/components/contents.tsx
+++ song-web/app/components/contents.tsx
@@ -47,61 +47,6 @@ function Intro() {
   );
 }
 
-// function Intro() {
-//   const introRef = useRef<HTMLDivElement>(null);
-
-//   const handleOnClick = () => {
-//     const intro = d3.select(introRef.current);
-//     intro.transition().duration(800).style('opacity', 0)
-//       .end().then(() => {
-//         intro.style('display', 'none');
-//       });
-//   }
-
-//   return (
-//     <div ref={introRef} className='intro intro--idle' onClick={handleOnClick}>
-//       <div className="intro-area intro-area--top">
-//       </div>
-//       <div className="intro__text-container intro__text-container--left">
-//         <div className="intro__text">
-//           <div className="intro-text__paragraph">
-//             <p className="text-style p">
-//               宋代刻本之美在于字体、板式。我们特地整理分析了宋刻板中楷书字体的发展时间史及江浙、四川、福建三地宋刻本中楷书字体的异同和字体倾向，以数据可视化的形式展示楷书及宋刻本书体演变史。
-//             </p>
-//           </div>
-//         </div>
-//         <div className="intro__date intro__date--large-screen">
-//           1368
-//         </div>
-//       </div>
-//       <div className="intro-area intro-area--left"></div>
-//       <div className="intro-area intro-area--bottom"></div>
-//       <div className="intro__text-container intro__text-container--right">
-//         <h2 className="text-style heading1">
-//           <span className="intro__text intro-text__title intro-text__title--first">
-//             楷书书体
-//           </span>
-//           <span className="intro__text intro-text__title intro-text__title--second">
-//             演变史
-//           </span>
-//         </h2>
-//         <h3 className="text-style heading3">
-//           <span className="intro__text intro-text__sub-title intro-text__sub-title--first">
-//             两宋时期
-//           </span>
-//           <span className="intro__text intro-text__sub-title intro-text__sub-title--second">
-//             宋版书
-//           </span>
-//         </h3>
-//         <div className="intro__date intro__date--large-screen">
-//           557
-//         </div>
-//       </div>
-//       <div className="intro-area intro-area--right"></div>
-//     </div>
-//   )
-// }
-
 function Story() {
   const events = LoadEvents();
   const maxPage = events.length;
